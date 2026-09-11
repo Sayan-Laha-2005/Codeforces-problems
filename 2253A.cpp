ce52892696860891c3fb237648457069eb733f7c
Sayan-Laha-2005 <sayanlahaclash@gmail.com>
2026-09-11 12:32:58 +0530

    Add Codeforces solutions

---
+++ 2253A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x) {
+    if (x < 2)
+        return false;
+
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (isPrime(n + 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
